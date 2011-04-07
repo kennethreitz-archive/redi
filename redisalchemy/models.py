05ae2e4c3575b9ab5217921425c6042b6dcfc8b3
Kenneth Reitz <me@kennethreitz.com>
2011-04-07 19:24:10 -0400

    redis

---
+++ redisalchemy/models.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__all__ = ('RList', 'RString', 'RSet')
+
+
+def RList(object):
+    """Redis list of awesomeness."""
+
+    def __init__(self, key):
+        pass
+
+    def append(value):
+        pass
+
+
+
+def RString(object):
+    """Redis string of awesomeness."""
+
+    def __init__(self, key):
+        pass
+
+
+
+def RSet(object):
+    """Redis string of awesomeness."""
+
+    def __init__(self, key):
+        pass
