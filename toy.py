6e864754f480a24947da82e735bcf5d53a6e1ea5
Kenneth Reitz <me@kennethreitz.com>
2011-04-08 18:22:25 -0400

    min.

--- toy.py
+++ toy.py
@@ -2,17 +2,8 @@
 
 import redi
 
+from redis import Redis
+
 v = redi.value('png')
 
-# with open('kr.png', 'rb') as f:
-    # v.value = f.read()
-    # f.write(v.value)
-# unicode(v.value)
-
-v.value = 'hi'
-
-# print type(v.value)
-print v.value
-
-# print repr(v.value)
-# print unicode(v.value)
+print repr(v.value)
