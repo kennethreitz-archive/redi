9b25bcbe4714116777fd93ef551cc85ecf17e238
Kenneth Reitz <me@kennethreitz.com>
2011-04-08 20:09:25 -0400

    document all the things

--- redi/core.py
+++ redi/core.py
@@ -14,7 +14,16 @@ from .config import init, redis
 
 
 def value(key, r=redis):
+    """Return RedisValue instance for given key.
+    Optional `r` keyword argument sets Redis instance.
+    """
+
     return RedisValue(key, r=r)
 
+
+
 def list(key, r=redis):
+    """Return RedisList instance for given key.
+    Optional `r` keyword argument sets Redis instance.
+    """
     return RedisList(key, r=r)
