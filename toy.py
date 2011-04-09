f908284dea2cfa6bda5b2d4e82bfb9f22dd97cc1
Kenneth Reitz <me@kennethreitz.com>
2011-04-09 14:30:52 -0400

    0.0.3

--- toy.py
+++ toy.py
@@ -7,7 +7,24 @@ lst = redi.list(('haystack', 'metalist'))
 # for l in lst:
     # print l
 
-lst[0].update({'a':'b'})
+# lst.delete()
+
+# lst.append(0)
+# lst.append(124)
+# lst.append(346)
+# lst.append(46)
+
+# print type(lst[1])
+lst[2] = 23.3
+
+# print type(lst[2])
+# print (lst[2])
+
+# print lst[2].keys()
+
+# lst[2]['face'] = ['book']
 
 print list(lst._raw)
+
+
 
