ac4fb5ed4a501107a10dcda0a8cf4cf4b90e818e
Kenneth Reitz <me@kennethreitz.com>
2011-04-10 20:38:25 -0400

    version bump 0.0.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ required = ['redis']
 
 setup(
     name='redi',
-    version='0.0.3',
+    version='0.0.4',
     description='Python middle layer for interacting with Redis data easily.',
     long_description=open('README.rst').read(),
     author='Kenneth Reitz',
